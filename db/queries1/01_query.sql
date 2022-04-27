eaa83c627d5bbcc14ded3b633856a3eb7623f1a0
Brenda <brendamvreal@gmail.com>
2022-04-26 22:41:00 -0400

    Delete extra files.

--- db/queries1/01_query.sql
+++
@@ -1,3 +0,0 @@
-SELECT is_completed, is_important, users.id as user_id
-FROM tasks
-JOIN users ON users.id = user_id;
--- db/queries1/02_query.sql
+++
@@ -1,6 +0,0 @@
-SELECT movies.name
-FROM movies
-JOIN categories ON categories.id = movies.category_id
-JOIN tasks ON tasks.category_id = categories.id
-JOIN users ON tasks.user_id = users.id
-WHERE users.id = 3;
