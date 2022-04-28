4243c6b1a044c8d343b9984750b655e6d6cf8bc4
Brenda <brendamvreal@gmail.com>
2022-04-28 19:08:50 -0400

    Install npm package.

---
+++ db/schema/06_books.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS books CASCADE;
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY NOT NULL,
+  category_id INTEGER REFERENCES categories(id),
+  rating SMALLINT NOT NULL DEFAULT 0,
+  name VARCHAR(255) NOT NULL,
+  year_created SMALLINT NOT NULL,
+  description TEXT,
+  img VARCHAR(255) NOT NULL,
+  author VARCHAR(255) NOT NULL,
+  task_id INTEGER REFERENCES tasks(id)
+);
